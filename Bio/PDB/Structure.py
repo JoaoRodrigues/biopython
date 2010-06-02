b1096c5fd608b691c137cafc8ff45e0fe9d48bb0
João Rodrigues <anaryin@gmail.com>
2010-06-01 19:50:47 -0700

    Added renumber_model function to Bio/PDB/Structure.py. Tested successfully with 2C2V.py (multiple chains, HETATMs).

--- Bio/PDB/Structure.py
+++ Bio/PDB/Structure.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2002, Thomas Hamelryck (thamelry@binf.ku.dk)
 # This code is part of the Biopython distribution and governed by its
 # license.  Please see the LICENSE file that should have been included
-# as part of this package.  
+# as part of this package.
 
 # My Stuff
 from Entity import Entity
@@ -17,41 +17,50 @@ class Structure(Entity):
     def __init__(self, id):
         self.level="S"
         Entity.__init__(self, id)
-
+    
     # Special methods
-
+    
     def __repr__(self):
         return "<Structure id=%s>" % self.get_id()
-
+    
     # Private methods
-
+    
     def _sort(self, m1, m2):
         """Sort models.
-
+        
         This sorting function sorts the Model instances in the Structure instance.
-        The sorting is done based on the model id, which is a simple int that 
+        The sorting is done based on the model id, which is a simple int that
         reflects the order of the models in the PDB file.
-
+        
         Arguments:
         o m1, m2 - Model instances
         """
         return cmp(m1.get_id(), m2.get_id())
-
-    # Public 
-
+    
+    # Public
+    
     def get_chains(self):
         for m in self:
             for c in m:
                 yield c
-
+    
     def get_residues(self):
         for c in self.get_chains():
             for r in c:
                 yield r
-
+    
     def get_atoms(self):
         for r in self.get_residues():
             for a in r:
                 yield a
+
+    def renumber_model(self):
+        """ Renumbers residues in a structure starting from 1. Keeps gaps."""
         
+        for m in self:
+            for c in m:
+                start = c.get_list()[0].get_id()[1]
+                displace = 1 - start
+                for r in c:
+                    r.id = (r.id[0], r.id[1]+displace, r.id[2])
 
