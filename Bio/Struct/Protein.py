f85545206adcd5da0ffbaab3cb4efbddf07050a5
João Rodrigues <anaryin@gmail.com>
2010-06-24 16:37:21 -0500

    Coarse Grain Feature

--- Bio/Struct/Protein.py
+++ Bio/Struct/Protein.py
@@ -56,65 +56,67 @@ class Protein(Structure):
             if cys_pair[0]['SG'] - cys_pair[1]['SG'] < threshold:
                 yield cys_pair
     
-def coarse_grain(ori_entity, cg_type="CA"):
-    """ 
-        Reduces the protein structure complexity to a few atoms per residue.
-        Arguments:
-        - ori_entity: Structure, Model, Chain, or List of Residues.
-        Parameters:
-        - cg_type:      CA (Ca-only) [Default]
-                        3pt (Ca + O + Side-chain C.o.M)
-        Returns a new structure object.
-    """
+    def coarse_grain(self, cg_type="CA"):
+        """ 
+            Reduces the protein structure complexity to a few atoms per residue.
+            Arguments:
+              - ori_entity: Structure, Model, Chain, or List of Residues.
+            Parameters:
+              - cg_type:      CA (Ca-only) [Default]
+                              3pt (Ca + O + Side-chain C.o.M)
+            Returns a new structure object.
+        """
     
-    from Bio.Struct.Geometry import center_of_mass
+        from Bio.Struct.Geometry import center_of_mass
     
-    entity = deepcopy(ori_entity)
-    
-    if isinstance(entity, Entity): # S,M,C
+        entity = deepcopy(self)
+        
         residue_list = entity.get_residues()
-    elif hasattr(entity, "__iter__") and filter(lambda x: x.level == 'R', entity): # list of R
-        residue_list = entity
-    else: # Some other weirdo object
-        raise ValueError('You can only use coarse grain on Structure, Model, Chain, or list of Residue(s).')
         
-    if cg_type == "CA":
-        for residue in residue_list:
-            for atom_name in residue.child_dict.keys():
-                if atom_name != 'CA':
-                    residue.detach_child(atom_name)
+        # if isinstance(entity, Entity): # S,M,C
+        #     residue_list = entity.get_residues()
+        # elif hasattr(entity, "__iter__") and filter(lambda x: x.level == 'R', entity): # list of R
+        #     residue_list = entity
+        # else: # Some other weirdo object
+        #     raise ValueError('You can only use coarse grain on Structure, Model, Chain, or list of Residue(s).')
+        
+        if cg_type == "CA":
+            for residue in residue_list:
+                for atom_name in residue.child_dict.keys():
+                    if atom_name != 'CA':
+                        residue.detach_child(atom_name)
     
-    elif cg_type == "3pt":
+        elif cg_type == "3pt":
         
-        from Bio.PDB.Atom import Atom # To add C.O.M
+            from Bio.PDB.Atom import Atom # To add C.O.M
         
-        for residue in residue_list:
+            for residue in residue_list:
 
-            side_chain_atoms = []
-            for atom in residue.child_dict.keys():
-                if atom in ['N', 'C']:
-                    residue.detach_child(atom)
-                elif atom not in ['CA', 'O']:
-                    side_chain_atoms.append(residue[atom])
-                    residue.detach_child(atom)
-            if len(side_chain_atoms) == 1: # Alanine
-                sc_com = side_chain_atoms[0]
-                sc_com.name = 'CMA'
-                residue.add(sc_com)                    
-            elif len(side_chain_atoms) > 1:
-                sc_com_coord = center_of_mass(side_chain_atoms)
-                if residue.resname in ['GLU', 'ASP']:
-                    sc_com = Atom('CMA', sc_com_coord, 0, 0, ' ', 'CMA', 0, 'O')
-                elif residue.resname in ['LYS', 'ARG', 'HIS']:
-                    sc_com = Atom('CMA', sc_com_coord, 0, 0, ' ', 'CMA', 0, 'N')
-                elif residue.resname == 'CYS':
-                    sc_com = Atom('CMA', sc_com_coord, 0, 0, ' ', 'CMA', 0, 'S')
-                else:
-                    sc_com = Atom('CMA', sc_com_coord, 0, 0, ' ', 'CMA', 0, 'C')
+                side_chain_atoms = []
+                for atom in residue.child_dict.keys():
+                    if atom in ['N', 'C']:
+                        residue.detach_child(atom)
+                    elif atom not in ['CA', 'O']:
+                        side_chain_atoms.append(residue[atom])
+                        residue.detach_child(atom)
+                if len(side_chain_atoms) == 1: # Alanine
+                    sc_com = side_chain_atoms[0]
+                    sc_com.name = 'CMA'
+                    residue.add(sc_com)                    
+                elif len(side_chain_atoms) > 1:
+                    sc_com_coord = center_of_mass(side_chain_atoms)
+                    if residue.resname in ['GLU', 'ASP']:
+                        sc_com = Atom('CMA', sc_com_coord, 0, 0, ' ', 'CMA', " ", 0, 'O')
+                    elif residue.resname in ['LYS', 'ARG', 'HIS']:
+                        sc_com = Atom('CMA', sc_com_coord, 0, 0, ' ', 'CMA', " ", 0, 'N')
+                    elif residue.resname == 'CYS':
+                        sc_com = Atom('CMA', sc_com_coord, 0, 0, ' ', 'CMA', " ", 0, 'S')
+                    else:
+                        sc_com = Atom('CMA', sc_com_coord, 0, 0, ' ', 'CMA', " ", 0, 'C')
 
-                residue.add(sc_com)
+                    residue.add(sc_com)
                           
-    else:
-        raise ValueError("Invalid complexity value: %s\nMust either be 1 or 3." %complexity)
+        else:
+            raise ValueError("Invalid Coarse Graining Type: %s. Check __doc__ for supported values." %cg_type)
         
-    return entity  
+        return entity  
