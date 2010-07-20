6701ce76f050ff86c3222db7babbf6fdc3dcd285
João Rodrigues <anaryin@gmail.com>
2010-07-20 17:19:23 +0200

    Changed severity of missing element to Exception.

--- Bio/Struct/Geometry.py
+++ Bio/Struct/Geometry.py
@@ -3,41 +3,52 @@
 # license.  Please see the LICENSE file that should have been included
 # as part of this package.
 
+"""
+Module with assorted geometrical functions on
+macromolecules.
+"""
+
 from Bio.PDB import Entity
 
 def center_of_mass(entity, geometric=False):
     """
-    Returns gravitic or geometric center of mass of an Entity.
+    Returns gravitic [default] or geometric center of mass of an Entity.
     Geometric assumes all masses are equal (geometric=True)
-    Defaults to Gravitic.
     """
     
-    if isinstance(entity, Entity.Entity): # Structure, Model, Chain, Residue
+    # Structure, Model, Chain, Residue
+    if isinstance(entity, Entity.Entity):
         atom_list = entity.get_atoms()
-    elif hasattr(entity, '__iter__') and filter(lambda x: x.level == 'A', entity): # List of Atoms
+    # List of Atoms
+    elif hasattr(entity, '__iter__') and [x for x in entity if x.level == 'A']:
         atom_list = entity
     else: # Some other weirdo object
-        raise ValueError('Center of Mass can only be calculated from the following objects:\n \
+        raise ValueError('Center of Mass can only be calculated \
+                            from the following objects:\n \
                             Structure, Model, Chain, Residue, list of Atoms.')
     
     masses = []
-    pos = [ [], [], [] ] # [ [X1, X2, ..] , [Y1, Y2, ...] , [Z1, Z2, ...] ]
+    positions = [ [], [], [] ] # [ [X1, X2, ..] , [Y1, Y2, ...] , [Z1, Z2, ...] ]
     
     for atom in atom_list:
         masses.append(atom.mass)
         
         for i, coord in enumerate(atom.coord.tolist()):
-            pos[i].append(coord)
+            positions[i].append(coord)
     
-    if '?' in set(masses): # If there is a single atom with undefined mass, default to Geometric
-        geometric = True
-        import warnings
-        from PDBExceptions import PDBConstructionWarning
-        warnings.warn("Some atoms have undefined masses. Calculating Geometric Center of Mass instead.",
-                      PDBConstructionWarning)
+    # If there is a single atom with undefined mass complain loudly.
+    if '?' in set(masses) and not geometric:
+        from Bio.PDB.PDBExceptions import PDBConstructionException
+        PDBConstructionException("Some atoms have undefined masses. \
+                                  Use Geometrical C.o.M. instead.")
     
     if geometric:
-        return map( lambda y: sum(y)/len(masses), pos)
-    else:
-        w_pos = map(lambda x: [x[i]*masses[i] for i in range(len(x))], pos)
-        return map(lambda y: sum(y)/sum(masses), w_pos)    
+        return [sum(coord_list)/len(masses) for coord_list in positions]
+    else:       
+        w_pos = [ [], [], [] ]
+        for atom_index, atom_mass in enumerate(masses):
+            w_pos[0].append(positions[0][atom_index]*atom_mass)
+            w_pos[1].append(positions[1][atom_index]*atom_mass)
+            w_pos[2].append(positions[2][atom_index]*atom_mass)
+
+        return [sum(coord_list)/sum(masses) for coord_list in w_pos]
